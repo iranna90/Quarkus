2a95afe56ab13cefbd360ebb1d6385743f3386e7
Iranna Patil <iranna.patil@irdeto.com>
2022-01-07 16:01:52 +0100

    Quarkus rest end point with Database access

---
+++ src/main/java/com/justexample/rest/json/User.java
@@ -0,0 +1,49 @@
+package com.justexample.rest.json;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+@Table(name = "user_data")
+public class User extends PanacheEntityBase {
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Id
+  @Column(name = "email")
+  private String email;
+
+  @JsonCreator
+  public User(@JsonProperty("first_name") final String firstName,
+              @JsonProperty("last_name") final String lastName,
+              @JsonProperty("email") final String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  protected User(){
+    // database
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
